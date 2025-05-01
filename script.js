b5b93cced2c861c6e05929d5cbe35bb70d9f9302
elbouazzaoui <elbouazzaouiali21@gail.com>
2025-05-01 22:23:50 +0200

    rajout dynamisme

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const spans = document.querySelectorAll("h1 span");
+  spans.forEach((span, index) => {
+    span.style.setProperty('--i', index + 1);
+  });
+});
